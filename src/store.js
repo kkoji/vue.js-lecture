492be502998c60da039fed501fee3ab97364575e
Kazuya Kojima <kkojima@improver.me>
2019-03-24 08:48:53 +0900

    サイドナビゲーション開閉用のデータ管理機能を実装

--- src/store.js
+++ src/store.js
@@ -5,12 +5,16 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    drawer: false
   },
   mutations: {
-
+    toggleSideMenu (state) {
+      state.drawer = !state.drawer
+    }
   },
   actions: {
-
+    toggleSideMenu ({ commit }) {
+      commit('toggleSideMenu')
+    }
   }
 })
