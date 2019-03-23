7d3a233ca89989e05730c1ac43a0a09869af105e
Kazuya Kojima <kkojima@improver.me>
2019-03-23 13:25:05 +0900

    router-linkのリンク先定義とリンク名を修正

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'home' }">ホーム</router-link> |
+      <router-link :to="{ name: 'about' }">このサイトについて</router-link>
     </div>
     <main>
       <router-view/>
