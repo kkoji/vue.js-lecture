3c1373a94d658af490b8a6c2c3a3166c415ac35f
Kazuya Kojima <kkojima@improver.me>
2019-03-23 17:25:55 +0900

    サイドナビゲーションの追加

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,26 @@
 <template>
   <v-app>
     <v-toolbar app>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>マイアドレス帳</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
     </v-toolbar>
+    <AdrSideNav/>
 
     <v-content>
-      <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import AdrSideNav from './components/AdrSideNav'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AdrSideNav
   },
   data () {
     return {
