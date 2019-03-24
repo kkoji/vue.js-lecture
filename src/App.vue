319f34e2f1ed36b8dd39955635f9b38ea2ab6847
Kazuya Kojima <kkojima@improver.me>
2019-03-24 09:13:49 +0900

    サイドナビゲーションの開閉を実装

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click.stop="openSideMenu"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span>マイアドレス帳</span>
       </v-toolbar-title>
@@ -25,6 +25,11 @@ export default {
   data () {
     return {
       //
+    }
+  },
+  methods: {
+    openSideMenu () {
+      this.$store.dispatch('toggleSideMenu')
     }
   }
 }
--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,13 +1,6 @@
 <template>
   <v-layout wrap style="height: 200px;">
-
-    <v-container>
-      <v-layout justify-center>
-        <v-btn color="pink" dark @click.stop="drawer = !drawer">Toggle</v-btn>
-      </v-layout>
-    </v-container>
-
-    <v-navigation-drawer v-model="drawer" absolute temporary>
+    <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
       <v-list class="pa-1">
         <v-list-tile avatar>
           <v-list-tile-avatar>
@@ -41,7 +34,6 @@
 export default {
   data () {
     return {
-      drawer: false,
       items: [
         { title: '連絡先一覧', icon: 'list' }
       ]
