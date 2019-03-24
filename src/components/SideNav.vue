12f52275f1762c1167b894992e7c2ae2f3b9dabd
Kazuya Kojima <kkojima@improver.me>
2019-03-24 11:33:37 +0900

    サイドメニューへのリンク追加

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,33 +1,31 @@
 <template>
-  <v-layout wrap style="height: 200px;">
-    <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
-      <v-list class="pa-1">
-        <v-list-tile avatar>
-          <v-list-tile-avatar>
-            <img src="https://avatars2.githubusercontent.com/u/1363954?s=460&v=4">
-          </v-list-tile-avatar>
+  <v-navigation-drawer v-model="$store.state.drawer" absolute temporary>
+    <v-list class="pa-1">
+      <v-list-tile avatar>
+        <v-list-tile-avatar>
+          <img src="https://avatars2.githubusercontent.com/u/1363954?s=460&v=4">
+        </v-list-tile-avatar>
 
-          <v-list-tile-content>
-            <v-list-tile-title>Kazuya Kojima</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+        <v-list-tile-content>
+          <v-list-tile-title>Kazuya Kojima</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
 
-      <v-list class="pt-0" dense>
-        <v-divider></v-divider>
+    <v-list class="pt-0" dense>
+      <v-divider></v-divider>
 
-        <v-list-tile v-for="item in items" :key="item.title" @click="">
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
+      <v-list-tile v-for="item in items" :key="item.title" :to='item.link'>
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
 
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-  </v-layout>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
@@ -35,7 +33,8 @@ export default {
   data () {
     return {
       items: [
-        { title: '連絡先一覧', icon: 'list' }
+        { title: 'ホーム', icon: 'home', link: { name: 'home'} },
+        { title: '連絡先一覧', icon: 'list', link: { name: 'addresses' } }
       ]
     }
   }
