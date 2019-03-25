ff1f965b5df5ba124b95995d18a614bd303c965a
Kazuya Kojima <kkojima@improver.me>
2019-03-25 14:14:12 +0900

    Firebaseのセットアップ

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyDc4c7mlhZT0IWH-tteNj5WGDIXcxG1cFY",
+  authDomain: "my-address-pj-78a96.firebaseapp.com",
+  databaseURL: "https://my-address-pj-78a96.firebaseio.com",
+  projectId: "my-address-pj-78a96",
+  storageBucket: "my-address-pj-78a96.appspot.com",
+  messagingSenderId: "406233405488"
+};
+firebase.initializeApp(config);
 
 new Vue({
   router,
