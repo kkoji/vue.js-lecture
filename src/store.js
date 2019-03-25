785e07710a46c07014e20c7cc77dc5f5c4806970
Kazuya Kojima <kkojima@improver.me>
2019-03-25 15:20:31 +0900

    ログイン機能を実装

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import firebase from 'firebase'
 
 Vue.use(Vuex)
 
@@ -17,6 +18,10 @@ export default new Vuex.Store({
     }
   },
   actions: {
+    login () {
+      const google_auth_provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithRedirect(google_auth_provider)
+    },
     toggleSideMenu ({ commit }) {
       commit('toggleSideMenu')
     },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,24 @@
 <template>
-  <HelloWorld />
+  <v-container text-xs-center justify-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>マイアドレス帳</h1>
+        <p>マイアドレス帳をご利用の方は、Googleアカウントでログインしてください。</p>
+      </v-flex>
+
+      <v-flex xs12 mt-5>
+        <v-btn color='info' @click="login">Googleアカウントでログイン</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import { mapActions } from 'vuex'
 
-  export default {
-    components: {
-      HelloWorld
-    }
+export default {
+  methods: {
+    ...mapActions(['login'])
   }
+}
 </script>
