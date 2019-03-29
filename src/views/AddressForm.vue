b709b2eb14912530b2018301cbbf0e81fbb2caca
Kazuya Kojima <kkojima@improver.me>
2019-03-29 22:04:25 +0900

    address_idがある場合のみフォームデータを復元するように修正

--- src/views/AddressForm.vue
+++ src/views/AddressForm.vue
@@ -28,6 +28,8 @@ import { mapActions } from 'vuex'
 
 export default {
   created () {
+    if (!this.$route.params.address_id) return
+
     const address = this.$store.getters.getAddressById(this.$route.params.address_id)
     if (address) {
       this.address = address
