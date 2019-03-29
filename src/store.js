aa9b3aee5b360bf1e7667afa5d443f203e8e5048
Kazuya Kojima <kkojima@improver.me>
2019-03-29 16:16:56 +0900

    データをCloud Firestoreに保存する処理を追加

--- src/store.js
+++ src/store.js
@@ -41,12 +41,14 @@ export default new Vuex.Store({
     toggleSideMenu ({ commit }) {
       commit('toggleSideMenu')
     },
-    addAddress ({ commit }, address) {
+    addAddress ({ getters, commit }, address) {
+      if (getters.uid) firebase.firestore().collection(`users/${getters.uid}/addresses`).add(address)
       commit('addAddress', address)
     }
   },
   getters: {
     userName: state => state.login_user ? state.login_user.displayName : '',
-    photoURL: state => state.login_user ? state.login_user.photoURL : ''
+    photoURL: state => state.login_user ? state.login_user.photoURL : '',
+    uid: state => state.login_user ? state.login_user.uid : null
   }
 })
